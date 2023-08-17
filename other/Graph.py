62ac90fcab94d84aac49bfbdb62980b15cb0f422
Nazar <you@example.com>
2023-08-17 16:41:02 +0200

    added graph realization

---
+++ other/Graph.py
@@ -0,0 +1,58 @@
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connected_to = {}
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connected_to[nbr] = weight
+
+    def __str__(self):
+        return f"{self.id} " + "connected to: " + f"{[x.id for x in self.connected_to]}"
+
+    def get_connections(self):
+        return self.connected_to.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, nbr):
+        return self.connected_to[nbr]
+
+
+class Graph:
+    def __init__(self):
+        self.vert_list = {}
+        self.vert_amount = 0
+
+    def add_vertex(self, key):
+        self.vert_amount += 1
+        new_vertex = Vertex(key=key)
+        self.vert_list[key] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_list:
+            return self.vert_list[n]
+        else:
+            return None
+
+    def __contains__(self, item):
+        return item in self.vert_list
+
+    def add_edge(self, f, t, cost=0):
+        if f not in self.vert_list:
+            nv = self.add_vertex(f)
+        if t not in self.vert_list:
+            nv = self.add_vertex(t)
+        self.vert_list[f].add_neighbor(self.vert_list[t], cost)
+
+    def get_vertices(self):
+        return self.vert_list.keys()
+
+    def __iter__(self):
+        return iter(self.vert_list.values())
+
+
+
+
+
---
+++ other/search_tree.py
@@ -0,0 +1,105 @@
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._get(key, currentNode.left_child)
+        else:
+            return self._get(key, currentNode.right_child)
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def put(self, key, value):
+        if self.root:
+            self._put(key, value, self.root)
+        else:
+            self.root = TreeNode(key, value)
+        self.size += 1
+
+    def _put(self, key, value, currentNode):
+        if key < currentNode.key:
+            if currentNode.has_left_child():
+                self._put(key, value, currentNode.left_child)
+            else:
+                currentNode.left_child = TreeNode(key, value, parent=currentNode)
+        else:
+            if currentNode.has_right_child():
+                self._put(key, value, currentNode.right_child)
+            else:
+                currentNode.right_child = TreeNode(key, value, parent=currentNode)
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __contains__(self, item):
+        if self._get(item, self.root):
+            return True
+        else:
+            return False
+        
+
+class TreeNode:
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
+
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_shild == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.right_child or self.left_child)
+
+    def has_any_children(self):
+        return self.right_child or self.left_child
+
+    def has_both_children(self):
+        return self.right_child and self.left_child
+
+    def replace_node_data(self, key, value, lc, rc):
+        self.key = key
+        self.payload = value
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
